aafa273630d54e6c859e9715d2634c6af3f4c43c
cqhaisenbao <fanyu9353@gmail.com>
2022-03-16 21:08:00 +0800

    ci: init

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { loadDevTools } from "jira-dev-tool";
 
-loadDevTools(() => ReactDOM.render(<App />, document.getElementById("root")));
+ReactDOM.render(<App />, document.getElementById("root"));
